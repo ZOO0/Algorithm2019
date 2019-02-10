f6f680969216c2d65805501b4f008e8bfecfe032
ZOO0 <jymememe030@gmail.com>
2019-02-10 22:04:00 +0900

    Q 1.7 업데이트

--- src/Q_1_7.java
+++ src/Q_1_7.java
@@ -11,5 +11,35 @@ public class Q_1_7 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		int[][] inputArr = { {1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15},{16,17,18,19,20},{21,22,23,24,25}};
+		
+		int[][] newArr = rotate90(inputArr);
+		
+		// 결과확인
+		for (int i = 0; i < newArr.length; i++) {
+			for (int j = 0; j < newArr.length; j++) {
+				System.out.println(newArr[i][j]);
+			}
+			System.out.println("----------");
+		}
+	}
+	
+	public static int[][] rotate90(int[][] arr) {
+		int[][] newArray = new int[arr.length][arr.length];
+		
+		// 기존 행렬을 세로로, 거꾸로(밑에서부터 위로) 다시 담는다.
+		// 시계 방향으로 회전.i, j 역할을 바꾸면 반시계 방향도 가능
+		
+		for (int i = 0; i < arr.length; i ++) {
+			int j = arr.length - 1;
+			int column = 0;
+			
+			while (j >= 0) {
+				newArray[i][column] = arr[j][i];
+				j--;
+				column ++;
+			}
+		}
+		return newArray;
 	}
 }
