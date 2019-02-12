044433be8a591d8945fe254fed332c46d74b27d6
ZOO0 <jymememe030@gmail.com>
2019-02-12 23:44:39 +0900

    문제 추가

--- src/Q_3_2.java
+++ src/Q_3_2.java
@@ -1,3 +1,9 @@
+/*
+*3.2* *스택Min* :
+기본적인 push와 pop 기능이 구현된 스택에서 최솟값을 반환하는 min함수를 추가하려고 한다. 어떻게 설계할 수 있겠는가? 
+push, pop, min연산은 모두 O(1) 시간에 동작해야 한다.
+*/
+
 public class Q_3_2 {
 
 	public class IntStack {
