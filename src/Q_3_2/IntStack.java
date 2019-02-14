0a22bc087c53dcd7605971f01451923825a10666
ZOO0 <33554712+ZOO0@users.noreply.github.com>
2019-02-14 18:26:28 +0900

    최솟값 데이터를 배열로 처리

--- src/Q_3_2/IntStack.java
+++ src/Q_3_2/IntStack.java
@@ -5,49 +5,46 @@ package Q_3_2;
 push, pop, min연산은 모두 O(1) 시간에 동작해야 한다.
 */
 
-	public class IntStack {
-		private int pointer; //포인터
-		private int[] stack; //스택
-		private int max; //최대 용량 
-		private int minimumValue; //최솟값 
+public class IntStack {
+	private int pointer;      //포인터
+	private int[] stack;      //스택
+	private int max;          //최대 용량 
+	private int[] minimumValue; //최솟값 
 		
-		//생성자
-		public IntStack(int capacity) {
-			pointer = 0;
-			stack = new int[capacity];
-			max = capacity;
-		}
-		
-		public int push(int x) {
-			if (pointer >= max) throw new OverflowException();
-			
-			// push 할 때 minimumValue 초기화 
-			if (pointer == 0 || x < minimumValue) {
-				minimumValue = x;
-			}
-						
-			return stack[pointer++] = x;
-		}
-		
-		public int pop() throws EmptyException {
-			if (pointer <= 0 ) throw new EmptyException();
-			
-			// pop 할 때 minimumValue 가 문젠데............
-			
-						
-			return stack[--pointer];
-		}
-		
-		public int min() {
-			return minimumValue;
-		}
-		
-		public class OverflowException extends RuntimeException {
-			public OverflowException() {}
-		}
-		
-		public class EmptyException extends RuntimeException {
-			public EmptyException() {}
-		}
+	//생성자
+	public IntStack(int capacity) {
+		pointer = 0;
+		stack = new int[capacity];
+		max = capacity;
 	}
-
+		
+	public int push(int x) {
+		if (pointer >= max) throw new OverflowException();
+		
+		// push 할 때 minimumValue 추가
+		if (pointer == 0 || x < minimumValue[pointer-1]) {
+			minimumValue[pointer] = x;
+		} else {
+			minimumValue[pointer] = minimumValue[pointer-1];
+		}
+		return stack[pointer++] = x;
+	}
+		
+	public int pop() throws EmptyException {
+		if (pointer <= 0 ) throw new EmptyException();
+		minimumValue[pointer] = null;
+		return stack[--pointer];
+	}
+		
+	public int min() {
+		return minimumValue;
+	}
+		
+	public class OverflowException extends RuntimeException {
+		public OverflowException() {}
+	}
+		
+	public class EmptyException extends RuntimeException {
+		public EmptyException() {}
+	}
+}
